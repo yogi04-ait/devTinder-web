5de6d1e964cf0e6d90fe3c794585efbbe800635d
Yogesh Aithani <aithaniyogesh007@gmail.com>
2025-02-14 21:27:44 +0530

    routes added

---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-300 ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">🧑‍💻 DevTinder</a>
+        </div>
+        <div className="flex-none mr-5 mb-2">
+          <div className="dropdown dropdown-end ">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            ></div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+            ></div>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
