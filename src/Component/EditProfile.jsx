f6b92b1dfd732492e132c66047deb532c19741fa
Yogesh Aithani <aithaniyogesh007@gmail.com>
2025-02-17 17:39:59 +0530

    import fixed

--- src/Component/EditProfile.jsx
+++ src/Component/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
